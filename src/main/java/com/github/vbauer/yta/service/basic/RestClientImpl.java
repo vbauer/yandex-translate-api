a7d31bb85179d832dd3954516fecdffb35bcd397
Vladislav Bauer <bauer.vlad@gmail.com>
2015-10-18 07:08:45 +0600

    Add extra configs for HTTP client

--- src/main/java/com/github/vbauer/yta/service/basic/RestClientImpl.java
+++ src/main/java/com/github/vbauer/yta/service/basic/RestClientImpl.java
@@ -12,7 +12,6 @@ import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
 
-import javax.net.ssl.SSLContext;
 import java.util.Map;
 
 /**
@@ -42,7 +41,10 @@ public class RestClientImpl implements RestClient {
 
             final String url = formatMethodUrl(method);
             final Map<String, Object> params = composeRequestParameters(parameters);
-            final HttpResponse<String> response = Unirest.post(url).fields(params).asString();
+            final HttpResponse<String> response =
+                Unirest.post(url)
+                    .fields(params)
+                    .asString();
 
             ApiStatus.check(response.getStatus());
 
@@ -80,18 +82,21 @@ public class RestClientImpl implements RestClient {
     }
 
     private static HttpClient createClient() throws Exception {
-        final SSLContext sslContext = SSLContexts.custom()
-            .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-            .build();
-        final SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        final SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+            SSLContexts.custom()
+                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                .build()
+        );
 
         final RequestConfig requestConfig = RequestConfig.custom()
             .setSocketTimeout(DEFAULT_TIMEOUT)
             .setConnectTimeout(DEFAULT_TIMEOUT)
+            .setConnectionRequestTimeout(DEFAULT_TIMEOUT)
             .setRedirectsEnabled(true)
             .build();
 
         return HttpClients.custom()
+            .setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
             .setSSLSocketFactory(socketFactory)
             .setDefaultRequestConfig(requestConfig)
             .build();
