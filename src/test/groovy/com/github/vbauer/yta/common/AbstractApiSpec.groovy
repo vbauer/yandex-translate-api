ec0096f115d902908edbf3d4c3544a141478f33c
Vladislav Bauer <bauer.vlad@gmail.com>
2016-04-05 02:46:56 +0600

    Add comments about AbstractApiSpec fields

--- src/test/groovy/com/github/vbauer/yta/common/AbstractApiSpec.groovy
+++ src/test/groovy/com/github/vbauer/yta/common/AbstractApiSpec.groovy
@@ -35,8 +35,15 @@ import spock.lang.Specification
 
 abstract class AbstractApiSpec extends Specification {
 
+    /**
+     * <b>Important:</b> Do not use this API key in your projects.
+     * It is necessary to test this library and could be changed.
+     */
     public static final KEY = "trnsl.1.1.20151012T201854Z.52731c6f05cd2ac8.f9ccae1aec912fb16879d4d89a2a40bbf1c802a3"
 
+    /**
+     * Shared "YTA" API service for testing purpose.
+     */
     @Shared api = new YTranslateApiImpl(KEY)
 
 }
